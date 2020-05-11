77d1fc3658c9f1e287e598fb268d020eb3ca289b
vishnu <srivishnu995@gmail.com>
2020-05-11 10:16:49 +0530

    color changed in card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'typeface-roboto'
-import { Paper, Card, withStyles, AppBar, createMuiTheme, ThemeProvider, Typography, Box, CardContent } from '@material-ui/core'
+import { Paper, Card, withStyles, AppBar, createMuiTheme, ThemeProvider, Typography, Link, Box, CardContent } from '@material-ui/core'
 import list_dat from './data'
 import { grey, orange, red } from '@material-ui/core/colors';
 
@@ -32,22 +32,29 @@ const useStyle = (style) => ({
     margin: style.spacing(3),
     width: 200,
     height: 175,
-    boxShadow: `0 1px 3px ${theme.palette.primary.light}, 0 1px 2px ${theme.palette.primary.light}`,
+    boxShadow: `0 1px 3px ${theme.palette.secondary.dark}, 0 1px 3px ${theme.palette.secondary.dark}`,
     transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
     cursor: 'pointer',
     '&:hover': {
-      boxShadow: `0 14px 28px ${theme.palette.primary.light}, 0 10px 10px ${theme.palette.primary.light}`
+      boxShadow: `0 14px 28px ${theme.palette.primary.light}, 0 10px 10px ${theme.palette.primary.light}`,
     }
   },
   img: {
     display: 'block',
     maxHeight: '200px',
-    maxWidth: '200px'
+    maxWidth: '200px',
+
   },
   CardContent: {
     margin: 0,
     padding: 0,
-  }
+  },
+  list: {
+    '&:hover': {
+      textDecoration: 'none',
+    }
+  },
+
 })
 
 class App extends React.Component {
@@ -61,10 +68,10 @@ class App extends React.Component {
       <div key={dat.id}>
         <Card className={classes.card}>
           <CardContent className={classes.CardContent}>
-            <a className={classes.list} href={`${dat.address}`} target="_blank" rel="noopener noreferrer">
+            <Link className={classes.list} href={`${dat.address}`} target="_blank" rel="noopener noreferrer">
               <img className={classes.img} src={require(`${dat.icon}`)} alt={dat.name} />
-            </a>
-            <Typography align="center" color="secondary" variant="h5">{dat.name}</Typography>
+              <Typography className={classes.cardName} align="center" color="secondary" variant="h5">{dat.name}</Typography>
+            </Link>
           </CardContent>
         </Card>
 
@@ -73,7 +80,7 @@ class App extends React.Component {
     return (
       <ThemeProvider theme={theme}>
         <Paper className={classes.paper}>
-          <AppBar color="primary">
+          <AppBar color='primary'>
             <Typography align="center" variant="h3">Pages</Typography>
           </AppBar>
           <Box className={classes.CardContainer}>
